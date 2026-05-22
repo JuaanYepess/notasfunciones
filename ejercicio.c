07a7f76efd650c3fc4fe5e6bcd9148d8e035e090
Juan Yepes <juan.yepes@udla.edu.ec>
2026-05-22 14:53:05 -0500

    size of agregado

--- ejercicio.c
+++ ejercicio.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 float notas[] = {6.5, 7.1, 6, 5, 8.1};
+float suma = 0;
+float promedio;
 
 int main() {
 
-    for (int i = 0; i < 5; i++) {
+    int cantidad = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < cantidad; i++) {
         printf("%.1f\n", notas[i]);
+        suma = suma + notas[i];
     }
+
+    promedio = suma / cantidad;
+
+    printf("Promedio: %.2f", promedio);
 
     return 0;
 }
